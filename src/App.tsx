c3423a773c452d12001a877af6787d99f6ac022f
Quang nguyen <quangnguyen@Quangs-MacBook-Pro.local>
2023-03-19 23:43:41 -0700

    add info section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Routes, Route, useNavigate, useLocation } from 'react-router-dom'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, Alert } from 'react-bootstrap'
 import Testament from './Testament'
 
 const Home: React.FC = () => (
   <div className="p-3">
-    <p>
+    <p className="mb-1">
       Kính chào quý độc giả: Cảm ơn quý vị đã chọn và đọc Kinh Thánh qua Bản Dịch 2011 này. Trước
       hết chúng tôi xin cảm ơn một số quý đôc giả đã cho chúng tôi những lời góp ý rất quý báu.
       Chúng tôi rất hoan hỉ ghi nhận và tiếp tục cải thiện để bản dịch Kinh Thánh này phản ánh được
@@ -20,9 +20,41 @@ const Home: React.FC = () => (
       Nay kính.
       <br />
       MS Đặng Ngọc Báu
-      <br />
-      kinhthanhbd2011@gmail.com
     </p>
+    <p>
+      <a href="mailto:kinhthanhbd2011@gmail.com" target="_blank" rel="noreferrer">
+        kinhthanhbd2011@gmail.com
+      </a>{' '}
+      |{' '}
+      <a href="https://www.facebook.com/bau.dang" target="_blank" rel="noreferrer">
+        Facebook
+      </a>
+    </p>
+    <Alert variant="info" className="rounded-0">
+      <p className="mb-0">
+        • Nếu các bạn muốn có trên iOS (như là iPhone, iPad,...) thì vui lòng tải xuống và sử dụng{' '}
+        <a
+          href="https://apps.apple.com/us/app/kinh-th%C3%A1nh-bd2011/id1405782410"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Kinh Thánh BD2011
+        </a>{' '}
+        cho iOS.
+      </p>
+      <p className="mb-0">
+        • Nếu các bạn muốn đọc trong điện thoại Android (như là Samsung, LG, Lenovo,...) thì vui
+        lòng tải xuống và sử dụng{' '}
+        <a
+          href="https://play.google.com/store/apps/details?id=org.kinhthanhbd2011"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Kinh Thánh BD2011
+        </a>{' '}
+        cho Android.
+      </p>
+    </Alert>
   </div>
 )
 
@@ -34,21 +66,18 @@ const Navigation: React.FC = () => {
   const isTanUoc = location.pathname === '/tan-uoc'
   return (
     <>
-      <Navbar variant="dark" expand="sm" fixed="top" className="bg-darkRed px-2">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link onClick={() => navigate('/')} active={isHome} disabled={isHome}>
-              Kinh Thánh BD2011
-            </Nav.Link>
-            <Nav.Link onClick={() => navigate('/cuu-uoc')} active={isCuuUoc} disabled={isCuuUoc}>
-              Cựu Ước
-            </Nav.Link>
-            <Nav.Link onClick={() => navigate('/tan-uoc')} active={isTanUoc} disabled={isTanUoc}>
-              Tân Ước
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+      <Navbar variant="dark" fixed="top" className="bg-darkRed px-2">
+        <Nav>
+          <Nav.Link onClick={() => navigate('/')} active={isHome} disabled={isHome}>
+            Kinh Thánh BD2011
+          </Nav.Link>
+          <Nav.Link onClick={() => navigate('/cuu-uoc')} active={isCuuUoc} disabled={isCuuUoc}>
+            Cựu Ước
+          </Nav.Link>
+          <Nav.Link onClick={() => navigate('/tan-uoc')} active={isTanUoc} disabled={isTanUoc}>
+            Tân Ước
+          </Nav.Link>
+        </Nav>
       </Navbar>
       <div style={{ height: '56px' }}></div>
     </>
