819ec47ba2844203832d487b9ef94b266a794e5c
Quang Nguyen <wan15112001@gmail.com>
2025-08-24 14:25:30 -0700

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
@@ -10,6 +11,16 @@ import { OldTestament } from './OldTestament'
 import { NewTestament } from './NewTestament'
 import { Home } from './Home'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 2,
+      staleTime: 1000 * 60 * 60, // 1 hour
+      gcTime: 1000 * 60 * 60 * 24, // 24 hours
+    },
+  },
+})
+
 const router = createBrowserRouter([
   { path: '/', element: <Home /> },
   { path: '/cuu-uoc', element: <OldTestament /> },
@@ -18,11 +29,13 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="App">
-      <div className="Tab">
-        <RouterProvider router={router} future={{ v7_startTransition: true }} />
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <div className="Tab">
+          <RouterProvider router={router} future={{ v7_startTransition: true }} />
+        </div>
+        <TabBar />
       </div>
-      <TabBar />
-    </div>
+    </QueryClientProvider>
   </React.StrictMode>
 )
