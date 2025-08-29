602b316025d7acbc88f99c65a20ed5afc872de62
Quang Nguyen <wan15112001@gmail.com>
2025-08-28 23:36:26 -0700

    introduce chapter modal

--- src/Read.tsx
+++ src/Read.tsx
@@ -1,40 +1,70 @@
 import Accordion from 'react-bootstrap/Accordion'
+import Button from 'react-bootstrap/Button'
+import CloseButton from 'react-bootstrap/CloseButton'
 import { Chapter, useBook, useOldTestament, useNewTestament } from './hooks'
 import type { Book } from './dataSource'
+import { useState } from 'react'
 
-const renderParagraphs = (paragraph: Chapter[], paragraphIndex: number) => (
-  <div key={paragraphIndex}>
-    {Object.entries(paragraph).map(([key, val], chapterIndex) => (
-      <div key={chapterIndex}>
-        <h5 style={{ marginBottom: '0.75rem' }}>{key}</h5>
-        <p style={{ marginBottom: '0' }}>{val as unknown as string}</p>
+const ChapterModal = ({
+  title,
+  chapter,
+  onClose,
+}: {
+  title: string
+  chapter: Chapter[][]
+  onClose: () => void
+}) => {
+  return (
+    <div className="ChapterModal">
+      <h1 className="Title">{title}</h1>
+      <div className="CloseButton">
+        <CloseButton onClick={onClose} />
       </div>
-    ))}
-  </div>
-)
+      {chapter.map((paragraph: Chapter[], paragraphIndex: number) => (
+        <div key={paragraphIndex}>
+          {Object.entries(paragraph).map(([key, val], chapterIndex) => (
+            <div key={chapterIndex}>
+              <h5>{key}</h5>
+              <p>{val as unknown as string}</p>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  )
+}
 
 const Chapters = ({ testamentId, book }: { testamentId: string; book: Book }) => {
+  const [chapter, setChapter] = useState<Chapter[][] | null>(null)
+  const [chapterNumber, setChapterNumber] = useState<number | null>(null)
   const { data } = useBook(testamentId, book.filename)
   if (!data) return null
   return (
-    <Accordion flush>
+    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '0.4rem', padding: '1rem' }}>
       {data.chapters.map((data: Chapter, index: number) => {
         const count = index + 1
-        const chapter: Array<Chapter[]> = data[count]
+        const chapter: Chapter[][] = data[count]
         return (
-          <Accordion.Item key={index} eventKey={index.toString()} style={{ borderRadius: 0 }}>
-            <Accordion.Header className="fw-bold">
-              <span>Chương {count}</span>
-            </Accordion.Header>
-            <Accordion.Body className="m-0 p-3 bg-light">
-              <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-                {chapter.map(renderParagraphs)}
-              </div>
-            </Accordion.Body>
-          </Accordion.Item>
+          <Button
+            key={index}
+            variant="light"
+            onClick={() => {
+              setChapter(chapter)
+              setChapterNumber(count)
+            }}
+          >
+            {count}
+          </Button>
         )
       })}
-    </Accordion>
+      {chapter && (
+        <ChapterModal
+          title={`${book.name} ${chapterNumber}`}
+          chapter={chapter}
+          onClose={() => setChapter(null)}
+        />
+      )}
+    </div>
   )
 }
 
